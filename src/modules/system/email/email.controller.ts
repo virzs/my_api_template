584c8542ff4a38be51a9aace2d611375ca3eee2c
vir <virs98@outlook.com>
2023-10-17 16:53:23 +0800

    :technologist: chore 接口文档

--- src/modules/system/email/email.controller.ts
+++ src/modules/system/email/email.controller.ts
@@ -1,18 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { SendEmailDto } from './dtos/send.dto';
-import { ApiBody, ApiOperation } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('邮件')
 @Controller('system/email')
 export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
+  @ApiOperation({ summary: '发送邮件' })
   @Post('send')
   sendEmail(@Body() body: SendEmailDto) {
     return this.emailService.sendEmail(body.email);
   }
 
-  @ApiOperation({ description: '获取注册验证码' })
+  @ApiOperation({ summary: '获取注册验证码' })
   @ApiBody({ type: SendEmailDto })
   @Post('register/captcha')
   registerCaptcha(@Body() body: SendEmailDto) {
