b8907fd01efa9614bd3fda26c590016c9bba4841
vir <virs98@outlook.com>
2024-02-29 17:49:17 +0800

    :bug: chore 注册用户名必填

--- src/modules/users/schemas/user.ts
+++ src/modules/users/schemas/user.ts
@@ -7,7 +7,7 @@ import { Resource } from 'src/modules/resource/schemas/resource';
 
 @Schema({ timestamps: true })
 export class User extends BaseSchema {
-  @Prop({ type: String, required: false, unique: true })
+  @Prop({ type: String, required: true, unique: true })
   username: string;
 
   @Prop({ type: String, required: true })
