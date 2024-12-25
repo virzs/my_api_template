5c70f0eade66cb6090ff79125273dfc577b44330
vir <virs98@outlook.com>
2024-12-25 14:40:43 +0800

    feat: :technologist: 优化清除历史数据log提示

--- src/public/service/cleanup.service.ts
+++ src/public/service/cleanup.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectConnection } from '@nestjs/mongoose';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { Connection } from 'mongoose';
@@ -8,19 +8,29 @@ import { Connection } from 'mongoose';
  */
 @Injectable()
 export class CleanupService {
+  private readonly logger = new Logger(CleanupService.name);
+
   constructor(@InjectConnection() private connection: Connection) {}
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async cleanupOldData() {
+    this.logger.log('开始清理过期数据...');
     const collections = await this.connection.db.collections();
     const sixtyDaysAgo = new Date();
     sixtyDaysAgo.setDate(sixtyDaysAgo.getDate() - 60);
 
     for (const collection of collections) {
-      await collection.deleteMany({
+      const result = await collection.deleteMany({
         isDelete: true,
         updatedAt: { $lt: sixtyDaysAgo },
       });
+
+      result.deletedCount > 0 &&
+        this.logger.log(
+          `集合 ${collection.collectionName} 清理完成，删除了 ${result.deletedCount} 条记录`,
+        );
     }
+
+    this.logger.log('所有集合清理完成');
   }
 }
